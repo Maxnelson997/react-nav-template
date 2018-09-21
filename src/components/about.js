e69bbac02677e9b163999c493a985f461e812236
Max <maxnelson997@gmail.com>
2018-09-21 11:45:03 -0600

    inset layout

--- src/components/about.js
+++ src/components/about.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 export default class About extends Component {
   render() {
     return (
-      <div className='about layout__content'>
+      <div className='about layout__content-inset'>
         about goes here
       </div>
     );
--- src/components/home.js
+++ src/components/home.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 export default class Home extends Component {
   render() {
     return (
-      <div className='home layout__under-header'>
+      <div className='home layout__content-inset'>
         <img className='home__image' src='https://source.unsplash.com/collection/2203755'/>
         <a className='home__link' target='_blank' href='https://github.com/Maxnelson997/react-nav-template'>VIEW ON GITHUB</a>
       </div>
