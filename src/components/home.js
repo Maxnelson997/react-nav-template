5f3e437fe6d84eb761cbd84de711dcb42d3e5731
Max <maxnelson997@gmail.com>
2018-09-21 11:20:10 -0600

    home grid and link

--- src/components/home.js
+++ src/components/home.js
@@ -5,6 +5,7 @@ export default class Home extends Component {
     return (
       <div className='home layout__under-header'>
         <img className='home__image' src='https://source.unsplash.com/collection/2203755'/>
+        <a className='home__link' href='https://github.com/Maxnelson997/react-nav-template'>VIEW ON GITHUB</a>
       </div>
     );
   }
