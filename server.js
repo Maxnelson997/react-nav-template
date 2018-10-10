8616e019f1891815efd9fe205f94c7bc8b6701c9
Max <maxnelson997@gmail.com>
2018-10-10 12:59:09 -0600

    removed ssl direct

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const port = process.env.PORT || 8080;
 const app = express();
-var secure = require('ssl-express-www');
-const cors = require('cors');
+// var secure = require('ssl-express-www');
+// const cors = require('cors');
 
-app.use(secure)
+// app.use(secure)
 
 // app.use(function(req, res, next) {
 //   res.header("Access-Control-Allow-Origin", "*");
@@ -12,7 +12,7 @@ app.use(secure)
 //   next();
 // });
 
-app.use(cors())
+// app.use(cors())
 
 app.use(express.static(__dirname + '/dist/'));
 app.get(/.*/, function (req, res) {
